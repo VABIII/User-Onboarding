1b9ffd21d2c3754e0af2a8867d5d26e28ac469b9
Your Name <tbengtson82@gmail.com>
2021-11-10 16:09:21 -0600

    Built formSchema

--- user-onboarding/src/validation/formSchema.js
+++ user-onboarding/src/validation/formSchema.js
@@ -1,4 +1,24 @@
+import * as yup from "yup";
 
+
+export const formSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .trim()
+        .required("First Name is required.")
+        .min(3, "First name should be at least 3 characters long."),
+    lastName: yup
+        .string()
+        .trim()
+        .required("Last name is required")
+        .min(3, "Last name should be at least 3 characters long."),
+    password: yup
+        .string()
+        .required("Password is required.")
+        .min(8, "Password must be at least 8 characters long."),
+    termsOfService: yup
+        .boolean()
+})
 
 
 
